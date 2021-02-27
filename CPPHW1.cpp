4b87806d96ba3079f7814290e8e54b6da771128c
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2021-02-27 15:22:23 -0800

    Issue#1 variables (#3)
    * Added includes
    
    * Added Main Function
    
    * Added Constant Variables

---
+++ CPPHW1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+int main(){
+    const long SECONDS_PER_MINUTE = 60L;
+    const long MINUTES_PER_HOUR = 60L;
+    const long HOURS_PER_DAY = 24L;
+}
